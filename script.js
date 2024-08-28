dfe3e2dcfe547cd55c0b33b0d9e187236d451725
PRAVEEN KUMAR <102923982+pra-vee-n@users.noreply.github.com>
2024-08-28 14:30:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Cafe Dash',
             appToken: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
             promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-            eventsDelay: 20000, // 20 seconds
-            attemptsNumber: 20,
+            timing: 20000, // 20 seconds
+            attempts: 20,
         },
         4: {
             name: 'Train Miner',
